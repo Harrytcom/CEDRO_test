bc880f20e4579f30899814dc4804122b49e568c1
Harry Tkachenko <tharry@yandex.ru>
2022-04-03 00:47:26 +0300

    added script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,17 @@
 console.log('Hello Cedro!')
+
+const menuTemplate = document.querySelector('#menu-template').content;
+const menuContainer = document.querySelector('.menu-container');
+const navigationMenu = menuTemplate.querySelector('.footer__info').cloneNode(true);
+
+const menuButton = document.querySelector('.menu__button');
+
+
+const openMenu = () => {
+  menuContainer.append(navigationMenu);
+}
+
+menuButton.addEventListener('click', () => {
+  openMenu();
+  console.log('click na knopku');
+});
