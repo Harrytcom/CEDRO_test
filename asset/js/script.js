5e03cf2f13c5c94db9863e16b951b36614aae1a8
Harry Tkachenko <tharry@yandex.ru>
2022-04-12 01:00:19 +0300

    added scroll template

--- asset/js/script.js
+++ asset/js/script.js
@@ -13,5 +13,35 @@ const openMenu = () => {
 
 menuButton.addEventListener('click', () => {
   openMenu();
-  console.log('click na knopku');
+  console.log('click on Navigation Burger');
 });
+
+
+// Scroll как у Aplle =)
+// обрабатываем скролл на всей странице
+document.addEventListener('scroll', function(event) {
+  // реагируем на событие
+  console.log(event);
+});
+
+// отловим все случаи, когда пользователь крутит колесо мыши
+// при наведенном на элемент курсоре
+let div = document.getElementsByTagName('div')[0];
+div.addEventListener('wheel', function(event) {
+  console.log(event);
+});
+
+window.addEventListener('wheel', function() {
+  document.getElementById('showScroll').innerHTML = this.scrollY + 'px';
+  console.log(document.getElementById('showScroll').innerHTML = this.scrollY + 'px')
+});
+
+document.addEventListener('scroll', function() {
+  // получаем высоту элемента, на котором произошло событие
+  console.log(this.scrollTop);
+});
+
+// document.addEventListener('wheel', function(event) {
+//   // останавливаем поведение по умолчанию, то есть прокрутку
+//   event.preventDefault();
+// });
